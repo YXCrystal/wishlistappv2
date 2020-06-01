725ccc058909b498e9d0360ddf0bc0a38403f037
YXCRYSTAL <yxcwuu@gmail.com>
2020-06-01 17:42:11 -0400

    Add searchWishlist action to see if a listing is already in the user's wishlist

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,12 +5,21 @@ export const getCurrentUser = user => {
 };
 
 export const addToWishlist = (listing, currentUser) => async dispatch => {
-	const { title, image, price, currency_code, description, url } = listing[0];
+	const {
+		title,
+		listing_id,
+		image,
+		price,
+		currency_code,
+		description,
+		url,
+	} = listing[0];
 	const { _id } = currentUser.data;
-	console.log(_id);
+
 	axios
 		.post(`/api/user/wishlist/${_id}`, {
 			title,
+			listing_id,
 			image,
 			price,
 			currency_code,
@@ -20,4 +29,26 @@ export const addToWishlist = (listing, currentUser) => async dispatch => {
 		.then(res => {
 			dispatch({ type: "ADD_WISHLIST", payload: res.data });
 		});
+};
+
+export const fetchWishlist = currentUser => async dispatch => {
+	const { _id } = currentUser.data;
+	const response = await axios.get(`/api/user/wishlist/${_id}`);
+
+	dispatch({ type: "FETCH_WISHLIST", payload: response.data });
+};
+
+export const searchWishlist = (currentUser, listing) => async (
+	dispatch,
+	getState
+) => {
+	await dispatch(fetchWishlist(currentUser));
+
+	const wishlist = getState().user.wishlist;
+
+	wishlist.forEach(({ listing_id }) => {
+		if (listing_id == listing[0].listing_id) {
+			return dispatch({ type: "SEARCH_WISHLIST", payload: true });
+		}
+	});
 };
